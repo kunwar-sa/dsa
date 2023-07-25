aedd7b0aab1695f9565d8b805e794ff538bd74cb
kpsingh <singhkunwarprakhar@gmail.com>
2023-07-25 10:55:10 +0530

    Both are incomplete

---
+++ P042 Truncate Sentence/TruncateSentence.java
@@ -0,0 +1,16 @@
+class TruncateSentence {
+
+    public static void main(String[] args) {
+
+        String s = "Hello how are you Contestant";
+        int k = 4;
+        String ans = truncateSentence(s, k);
+        System.out.println(ans);
+        // Hello how are you
+    }
+
+    public static String truncateSentence(String s, int k) {
+
+        return null;
+    }
+}
