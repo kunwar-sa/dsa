fa6213f48ac5cf9e647673752ee19bee1942eac4
kpsingh <singhkunwarprakhar@gmail.com>
2023-08-11 13:39:38 +0530

    Package name change

---
+++ P004RemoveAllOccurencesOfSubstring/RemoveAllOccurencesOfSubstring.java
@@ -0,0 +1,23 @@
+package P004RemoveAllOccurencesOfSubstring;
+
+public class RemoveAllOccurencesOfSubstring {
+
+	public static void main(String[] args) {
+
+		System.out.println(removeOccurrences("we will rock you", "il"));
+	}
+
+	public static String removeOccurrences(String s, String part) {
+		boolean flag = true;
+		while (flag) {
+			if (s.contains(part))
+				s = s.replaceFirst(part, "");
+			else
+				flag = false;
+		}
+		return s;
+	}
+
+}
+
+/* leetcode */
