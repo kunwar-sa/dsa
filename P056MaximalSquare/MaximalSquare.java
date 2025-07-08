4beed27fd35157cf0181a8a1012d11de1319c548
kunwar-sa <singhkunwarprakhar@gmail.com>
2025-07-08 13:16:09 +0530

    Maximal square: commit this to memory. Asked in Google.

---
+++ P056MaximalSquare/MaximalSquare.java
@@ -0,0 +1,41 @@
+package P056MaximalSquare;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximalSquare {
+
+    public record pair(int x, int y) {};
+    
+    public int maximalSquare(char[][] matrix) {
+
+        Map<Record, Integer> map = new HashMap<>();
+        solve(matrix, map, 0, 0);
+        return (int)Math.pow((map.values().stream().max(Integer::compareTo).get()), 2);
+    }
+
+    public int solve(char[][] mat, Map<Record, Integer> map, int r, int c) {
+
+        if(r>=mat.length || c>=mat[0].length) {
+            return 0;
+        }
+
+        if(!map.containsKey(new pair(r, c))) {
+            int down = solve(mat, map, r+1, c);
+            int right = solve(mat, map, r, c+1);
+            int diag = solve(mat, map, r+1, c+1);
+
+            map.put(new pair(r,c), 0);
+            
+            if(mat[r][c] == '1') {
+                map.put(new pair(r,c), 1+Math.min(down, Math.min(right, diag)));
+            }
+            
+        }
+
+        return map.get(new pair(r, c));
+
+    }
+
+
+}
