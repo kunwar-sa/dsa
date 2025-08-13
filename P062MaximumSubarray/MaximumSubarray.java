15e6f903ec1704bb9e4e75c56cb7eac312891939
kunwar-sa <singhkunwarprakhar@gmail.com>
2025-08-13 18:42:19 +0530

    Add implementation for Maximum Subarray problem

---
+++ P062MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,17 @@
+package P062MaximumSubarray;
+
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] arr) {
+
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
